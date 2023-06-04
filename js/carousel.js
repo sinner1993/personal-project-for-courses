2f01285d170182f93cd2d472bad5118673b1ac34
Volodymyr Biloholovskyi <sergio1993goga@gmail.com>
2023-06-04 16:29:42 +0300

    saddf

--- js/carousel.js
+++ js/carousel.js
@@ -26,19 +26,52 @@ const slideLeft = document.querySelector(".gallery__left__button");
 const imgSelectors = document.querySelectorAll(".gallery__img__wrapper");
 const carouselWrapper = document.querySelector(".wrapper__for__carousel");
 const im = document.getElementById("1");
-let counter = 1;
-slideRight.addEventListener("click", ()=>{
-const imgWrapper  = document.getElementById(counter);
-carouselWrapper.scrollBy(carouselWrapper.scrollWidth, 0);
-imgSelectors.forEach((el,index)=>{ 
-    if(index == counter){
-        const clonedEl = imgWrapper.cloneNode(true); 
-        carouselWrapper.append(clonedEl);      
-    }
-})
-counter++
-counter == 4 ? counter = 1 : counter;
-})
+const imgItemWrapper = document.querySelector(".gallery__img__wrapper");
+
+const firstChild = document.getElementById(1);
+const lastChild = document.getElementById(4);
+let counter  = 1;
+  slideRight.addEventListener("click", ()=>{
+    let arr = Array.from(imgSelectors);
+    arr.forEach((element, index) => {
+        if(index == counter){
+            console.log("21453");
+            const copyFirst = element.cloneNode(true);
+            carouselWrapper.append(copyFirst);
+            carouselWrapper.scrollBy( imgItemWrapper.offsetWidth, 0);
+        }
+    });
+    counter++
+    counter = 4 ? 0 : counter;
+  })
+  slideLeft.addEventListener("click", ()=>{
+    let arr = Array.from(imgSelectors);
+    arr.forEach((element, index) => {
+        if(index == counter){
+            console.log("EFG");
+            const copyLast = element.cloneNode(true);
+            console.log(copyLast,"Sda");
+            carouselWrapper.prepend(copyLast);
+            carouselWrapper.scrollBy(imgItemWrapper, 0);
+        }
+    });
+    console.log(arr);
+    counter++
+    counter = 4 ? 0 : counter;
+  })
 
 
 
+  const slider = () => {
+    const copyFirst = firstChild.cloneNode(true);
+    const copyLast = lastChild.cloneNode(true);
+    console.log(firstChild);
+    carouselWrapper.append(copyFirst);
+    carouselWrapper.prepend(copyLast);
+    let position = imgItemWrapper.offsetWidth * 2
+    carouselWrapper.scrollLeft = position
+     // Adjust the value as per your requirement
+   
+  };
+  
+  slider();
