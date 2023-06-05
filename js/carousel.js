54f2d97aefd279b96b69e886cff0a4712d1a4b62
Volodymyr Biloholovskyi <sergio1993goga@gmail.com>
2023-06-05 23:52:04 +0300

    commit

--- js/carousel.js
+++ js/carousel.js
@@ -25,53 +25,36 @@ const slideRight = document.querySelector(".gallery__right__button");
 const slideLeft = document.querySelector(".gallery__left__button");
 const imgSelectors = document.querySelectorAll(".gallery__img__wrapper");
 const carouselWrapper = document.querySelector(".wrapper__for__carousel");
-const im = document.getElementById("1");
 const imgItemWrapper = document.querySelector(".gallery__img__wrapper");
+const sliderContainer = document.querySelector(".slider__contaienr");
+let initialCounterLeft = 3;
+let initialCounterRight  = 0;
+let translatedWidth = 0;
+let translatedWidthRight = 0;
 
-const firstChild = document.getElementById(1);
-const lastChild = document.getElementById(4);
-let counter  = 1;
   slideRight.addEventListener("click", ()=>{
-    let arr = Array.from(imgSelectors);
-    arr.forEach((element, index) => {
-        if(index == counter){
-            console.log("21453");
-            const copyFirst = element.cloneNode(true);
-            carouselWrapper.append(copyFirst);
-            carouselWrapper.scrollBy( imgItemWrapper.offsetWidth, 0);
-        }
-    });
-    counter++
-    counter = 4 ? 0 : counter;
+   translatedWidth -= imgItemWrapper.offsetWidth;
+   if(initialCounterRight < 0){
+       sliderContainer.style.transform = `translateX(${-translatedWidth}px)`;
+       sliderContainer.style.transition = `transform 300ms ease-in-out`;
+   }
+    
   })
-  slideLeft.addEventListener("click", ()=>{
-    let arr = Array.from(imgSelectors);
-    arr.forEach((element, index) => {
-        if(index == counter){
-            console.log("EFG");
-            const copyLast = element.cloneNode(true);
-            console.log(copyLast,"Sda");
-            carouselWrapper.prepend(copyLast);
-            carouselWrapper.scrollBy(imgItemWrapper, 0);
+  slideLeft.addEventListener("click", ()=>{ 
+    const selec = document.querySelectorAll(".gallery__img__wrapper");
+     translatedWidth += imgItemWrapper.offsetWidth;
+    const lastChild = imgSelectors[2].cloneNode(true);
+  selec.forEach((el, index)=>{     
+        if(initialCounterLeft == index){
+                sliderContainer.style.transform = `translateX(-${translatedWidth}px)`;
+                sliderContainer.style.transition = `transform 300ms ease-in-out`;
+                sliderContainer.append(lastChild); 
         }
-    });
-    console.log(arr);
-    counter++
-    counter = 4 ? 0 : counter;
+        
+    })
+    initialCounterLeft--
+    initialCounterRight--
+    console.log(initialCounterRight,"123456");
+    initialCounterLeft = -1 ? 3 : initialCounterLeft; 
   })
 
-
-
-  const slider = () => {
-    const copyFirst = firstChild.cloneNode(true);
-    const copyLast = lastChild.cloneNode(true);
-    console.log(firstChild);
-    carouselWrapper.append(copyFirst);
-    carouselWrapper.prepend(copyLast);
-    let position = imgItemWrapper.offsetWidth * 2
-    carouselWrapper.scrollLeft = position
-     // Adjust the value as per your requirement
-   
-  };
-  
-  slider();
